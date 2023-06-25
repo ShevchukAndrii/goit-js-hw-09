7c7d747d5ca476e77f0b6ec80ee7d8ff265cb13b
ShevchukAndrii <123541026+ShevchukAndrii@users.noreply.github.com>
2023-06-25 21:31:41 +0000

    Deploying to gh-pages from @ ShevchukAndrii/goit-js-hw-09@2fb79bff6f69170901f200095ba438c7d1a467ad 🚀

---
+++ 01-color-switcher.eb949c87.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;e.addEventListener("click",(()=>{a=setInterval((()=>{const a=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=a,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.eb949c87.js.map
