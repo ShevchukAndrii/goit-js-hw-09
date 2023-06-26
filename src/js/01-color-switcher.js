6beedd046a9488290779066c48aa7d537d17792c
ShevchukAndrii <andrefanchik@gmail.com>
2023-06-26 23:14:02 +0300

    xxx

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,16 +2,18 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const bodyEl = document.querySelector('body');
 let timerId = null;
+stopBtn.disabled = true;
 
 const getRandomHexColor= () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 const changeColorBody = () => {
+  startBtn.disabled = true;
+         stopBtn.disabled = false;
      timerId = setInterval(() => {
         const color = getRandomHexColor();
          bodyEl.style.backgroundColor = color;
-         startBtn.disabled = true;
-         stopBtn.disabled = false;
+         
     }, 1000);
 
 }
